4330597a6dccc089eb3ae9e493cda4900720ab04
Zicophs <51864963+Zicophs@users.noreply.github.com>
2023-11-14 17:22:18 -0300

    Ahora anda

--- proyecto/src/components/Navigation.js
+++ proyecto/src/components/Navigation.js
@@ -10,93 +10,67 @@ import EventScreen from "../screens/EventScreen";
 import NextEventsScreen from "../screens/NextEventsScreen";
 import PastEventsScreen from "../screens/PastEventsScreen";
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-const DetailScreen = () => {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen name="Detail Event" component={EventScreen} />
-        {/* Otras pantallas de Detail Event si las tienes */}
-      </Stack.Navigator>
-    );
-  };
+const HomeTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
+            if (route.name === 'Home') {
+                iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Next Events') {
+                iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+            }else if (route.name === 'Past Events') {
+              iconName = focused ? 'ios-folder' : 'ios-folder-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#fdfd9b',
+          tabBarInactiveTintColor: 'white',
+          tabBarActiveBackgroundColor: 'grey',
+          tabBarInactiveBackgroundColor: 'grey',
+      })}
+    >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Next Events" component={NextEventsScreen} />
+        <Tab.Screen name="Past Events" component={PastEventsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const DetailStackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Detail Event"
+        component={EventScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 function Navigation () {
-    return (
-        <NavigationContainer>
-            {/*<Stack.Navigator >
-                <Stack.Screen name="HomeScreen" component={HomeScreen} />
-                <Stack.Screen name="LoginScreen" component={LoginScreen} />
-                <Stack.Screen name="RegistrationScreen" component={RegisterScreen} />
-                <Stack.Screen name="NextEventsScreen" component={NextEventsScreen} />
-                <Stack.Screen name="PastEventsScreen" component={PastEventsScreen} />
-                <Stack.Screen name="EventScreen" component={EventScreen}/>
-    </Stack.Navigator>*/}
-            <Tab.Navigator
-              screenOptions={({ route }) => ({
-                  tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-        
-                    if (route.name === 'Home') {
-                        iconName = focused
-                        ? 'ios-home'
-                        : 'ios-home-outline';
-                    } else if (route.name === 'Next Events') {
-                        iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
-                    }else if (route.name === 'Past Events') {
-                      iconName = focused ? 'ios-folder' : 'ios-folder-outline';
-                    }
-        
-                    // You can return any component that you like here!
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                  },
-                  tabBarActiveTintColor: '#fdfd9b',
-                  tabBarInactiveTintColor: 'white',
-                  tabBarActiveBackgroundColor: 'grey',
-                  tabBarInactiveBackgroundColor: 'grey',
-              })}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Next Events" component={NextEventsScreen} />
-                <Tab.Screen name="Past Events" component={PastEventsScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeTabNavigator"
+          component={HomeTabNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="DetailStackScreen" component={DetailStackScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
 
 export default Navigation;
-
-    /*return (
-      <NavigationContainer>
-          <Tab.Navigator
-              screenOptions={({ route }) => ({
-                  tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-        
-                    if (route.name === 'Home') {
-                        iconName = focused
-                        ? 'ios-home'
-                        : 'ios-home-outline';
-                    } else if (route.name === 'Next Events') {
-                        iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
-                    }else if (route.name === 'Past Events') {
-                      iconName = focused ? 'ios-folder' : 'ios-folder-outline';
-                    }
-        
-                    // You can return any component that you like here!
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                  },
-                  tabBarActiveTintColor: '#fdfd9b',
-                  tabBarInactiveTintColor: 'white',
-                  tabBarActiveBackgroundColor: 'grey',
-                  tabBarInactiveBackgroundColor: 'grey',
-              })}
-          >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Next Events" component={NextEventsScreen} />
-            <Tab.Screen name="Past Events" component={PastEventsScreen} />
-            </Tab.Navigator>
-      </NavigationContainer>
-    );*/
