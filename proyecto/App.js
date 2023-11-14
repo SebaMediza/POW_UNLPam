e529fe96f0664271f72c512a1081ad3c11d4584d
Zicophs <51864963+Zicophs@users.noreply.github.com>
2023-11-14 17:02:23 -0300

    a

--- proyecto/App.js
+++ proyecto/App.js
@@ -13,38 +13,6 @@ function App () {
   return (
     <Navigation></Navigation>
   );
-    /*return (
-      <NavigationContainer>
-          <Tab.Navigator
-              screenOptions={({ route }) => ({
-                  tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-        
-                    if (route.name === 'Home') {
-                        iconName = focused
-                        ? 'ios-home'
-                        : 'ios-home-outline';
-                    } else if (route.name === 'Next Events') {
-                        iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
-                    }else if (route.name === 'Past Events') {
-                      iconName = focused ? 'ios-folder' : 'ios-folder-outline';
-                    }
-        
-                    // You can return any component that you like here!
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                  },
-                  tabBarActiveTintColor: '#fdfd9b',
-                  tabBarInactiveTintColor: 'white',
-                  tabBarActiveBackgroundColor: 'grey',
-                  tabBarInactiveBackgroundColor: 'grey',
-              })}
-          >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Next Events" component={NextEventsScreen} />
-            <Tab.Screen name="Past Events" component={PastEventsScreen} />
-            </Tab.Navigator>
-      </NavigationContainer>
-    );*/
 }
 
 export default App;
