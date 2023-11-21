3fb7de48f485ae39c2b351636aa57416e2b0a87f
SebaMediza <sebamediza@hotmail.com>
2023-11-20 22:37:16 -0300

    Refactor search functionality in peliculas page

--- bananatv/app/pages/peliculas/page.jsx
+++ bananatv/app/pages/peliculas/page.jsx
@@ -4,13 +4,14 @@ import { useEffect, useState } from "react"
 import '../../../public/assets/css/Card.css'
 import Card from "@/components/Card"
 
-const page = ({isLogged}) => {
+const page = ({}) => {
     const api = 'http://localhost:7071/peliculas'
     const getPelis = async () => {
         const res = await fetch(api);
         const data = await res.json();
         console.log(data.data);
         setData(data.data);
+        setSearchData(data.data);
         setLoading(false);
     }
 
@@ -19,8 +20,25 @@ const page = ({isLogged}) => {
     }, []);
 
     const [data, setData] = useState([]); //useState([]) es el estado inicial, que es un array vacio
+    const [searchData, setSearchData] = useState([]); //useState([]) es el estado inicial, que es un array vacio
+    const [searchTerm, setSearchTerm] = useState(""); //useState("") es el estado inicial, que es un string vacio
     const [loading, setLoading] = useState(true); //useState(false) es el estado inicial, que es un booleano en false
     const [error, setError] = useState(null); //useState(null) es el estado inicial, que es un null
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+        filtrar(e.target.value);
+    }
+
+    const filtrar = (terminoBusqueda) => {
+        var resultadosBusqueda = searchData.filter((elemento) => {
+            if (elemento.titulo.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+            ) {
+                return elemento;
+            }
+        });
+        setData(resultadosBusqueda);
+    }
 
 
     return (
@@ -30,7 +48,14 @@ const page = ({isLogged}) => {
                     <div className="title">
                         <h1>Peliculas</h1>
                     </div>
-                        <Card producciones={data} />
+                    <div className="busqueda">
+                        <input
+                            type="text"
+                            value={searchTerm}
+                            placeholder="Búsqueda por Nombre o Empresa"
+                            onChange={handleChange} />
+                    </div>
+                    <Card producciones={data} />
                 </div>
             )}
         </>
