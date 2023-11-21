69845a0933914550d6721a09d6b1f41e6929ce62
bgebruers <blas.gebruers@gmail.com>
2023-11-21 17:29:23 -0300

    ultima api con seguridad

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const {API_PORT} = process.env;
-const bcrypt = require("bcryptjs");
+
 
 const usuarios = require('./controllers/usuario-controller')
 const auth = require("./middleware/auth");
@@ -29,9 +29,41 @@ app.post("/registro", usuarios.registro);
 app.post("/login", usuarios.login);
 
 //ruta que se puede acceder si estas loguado
-app.get("/solologueado", auth,(req, res) => {
-
+app.get("/logueados", auth,(req, res) => {
+  
   res.json({ message: "Hola soy la api de node" });
+});
+
+
+//configura el header para que llegue el token desde la bd al middwere
+app.use((req, res, next) => {
+  
+  // Configura el encabezado para permitir solicitudes desde cualquier origen
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // Configura el encabezado para permitir ciertos métodos HTTP
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+  // Configura el encabezado para permitir ciertos encabezados en las solicitudes
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  // Añade el encabezado necesario para la autenticación
+  res.header('x-access-token', global.myAppToken);
+  const miCampo = req.headers['x-access-token'];
+
+  console.log("el campo del header queda: " + miCampo);
+  // Continúa con el siguiente middleware o manejador de ruta
+  next();
+});
+
+
+//ruta para cerrar sesion. Antes de poder cerrar sesion chequea si tiene token es decir si esta logeado.
+app.get("/cerrarSesion",auth,(req, res)=>{
+  if (res && res.statusCode === 200) {
+    usuarios.cerrarSesion(req.body.idUser, res);
+  }else{
+    console.log("error respondio mal"+  res.status);
+  }
 });
 
 
