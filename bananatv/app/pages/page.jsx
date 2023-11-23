5cd18ce7cbe005b481e37f425aebb19593f885d2
SebaMediza <sebamediza@hotmail.com>
2023-11-23 16:42:14 -0300

    Update live TV and main page

--- bananatv/app/pages/page.jsx
+++ bananatv/app/pages/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '@/public/assets/css/LandPage.css'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='landPage'>
+      <h1>Bienbenido a BananaTV</h1>
+      <img src="/img/logo.jpg" alt="" />
+      <h3>Plataforma de Streaming de Series Y Peliculas</h3>
+      <p>BananaTV se encuentra en fase de desarrollo Beta, por lo que algunas funciones no estaran disponilbes en este momento.</p>
+      <p>Pedimos disculpas por los incovenientes.</p>
+      <p>El equipo de desarrollo de BananaTV</p>
+      <p>Mediza Sebastián, Pereyra Federico, Gerbruers Blas</p>
+    </div>
   )
 }
 
