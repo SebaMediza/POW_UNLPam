3fed62ea15904ad82fdf520fc84840e54a4a33c3
SebaMediza <sebamediza@hotmail.com>
2023-11-24 13:14:51 -0300

    Refactor HomeScreen component to fetch peliculas data

--- proyecto/src/screens/HomeScreen.js
+++ proyecto/src/screens/HomeScreen.js
@@ -1,69 +1,65 @@
-import React from "react";
 import { Button, Text, View, StyleSheet, FlatList } from "react-native";
+import React, { useEffect, useState } from 'react';
 
-function HomeScreen ({navigation}) {
-    const data = [
-        { id: '1', text: 'Elemento 1' },
-        { id: '2', text: 'Elemento 2' },
-        { id: '3', text: 'Elemento 3' },
-        { id: '4', text: 'Elemento 4' },
-        { id: '5', text: 'Elemento 5' },
-        // Agrega más elementos según sea necesario
-      ];
+function HomeScreen({ navigation }) {
+  const [peliculas, setPeliculas] = useState(null);
 
-    return (
+  useEffect(() => {
+    // Función asincrónica para realizar la solicitud
+    const fetchData = async () => {
+      try {
+        // Hacer la solicitud usando fetch
+        const response = await fetch('http://localhost:7071/peliculas', {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJibGFzIiwiaWF0IjoxNzAwNjc3NjY4fQ.iUrd1YhX5F0BILCPmNaIFteREZndbmSDpAuuoY5af-Y'
+          }
+        });
+
+        // Verificar si la respuesta es exitosa (código de estado 200)
+        if (response.ok) {
+          // Parsear la respuesta como JSON
+          const result = await response.json();
+
+          // Actualizar el estado con los datos recibidos
+          setPeliculas(result);
+        } else {
+          // Manejar errores en la respuesta
+          console.error('Error en la solicitud:', response.statusText);
+        }
+      } catch (error) {
+        // Manejar errores de red u otros errores
+        console.error('Error en la solicitud:', error.message);
+      }
+    };
+
+    // Llamar a la función de solicitud cuando el componente se monta
+    fetchData();
+  }, []); // La dependencia vacía asegura que useEffect se ejecute solo una vez al montar el componente
+  {console.log(peliculas)}
+
+  return (
+
+    <View style={styles.container}>
       
-        <View style={styles.container}>
-            <View style={styles.section}>
-                <Text style={styles.sectionTitle}>Next Events</Text>
-                <FlatList
-                    data={data}
-                    keyExtractor={(item) => item.id}
-                    horizontal={true} // Configura la lista como horizontal
-                    showsHorizontalScrollIndicator={false}
-                    renderItem={({ item }) => (     
-                      <View style={{ margin: 10, padding: 50, backgroundColor: 'lightgray' }}>
-                          <Text>{item.text}</Text>
-                          <Button title="Details" onPress={()=> navigation.navigate("DetailStackScreen")}></Button>
-                      </View>                     
-                    )}
-                />
-                <Button title="More Next Events" color={"black"} onPress={() => navigation.navigate('Next Events')}></Button>                    
-            </View>
-            <View style={styles.section}>
-                <Text style={styles.sectionTitle}>Past Events</Text>
-                <FlatList
-                    data={data}
-                    keyExtractor={(item) => item.id}
-                    horizontal={true} // Configura la lista como horizontal
-                    showsHorizontalScrollIndicator={false}
-                    renderItem={({ item }) => (
-                      <View style={{ margin: 10, padding: 50, backgroundColor: 'lightgray' }}>
-                          <Text>{item.text}</Text>
-                          <Button title="Details" onPress={()=> navigation.navigate("DetailStackScreen")}></Button>
-                      </View>
-                    )}
-                />
-                <Button title="More Past Events" color={"black"} onPress={() => navigation.navigate('Past Events')}></Button>
-            </View>
-        </View>
-      
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-    },
-    section: {
-      marginBottom: 20,
-    },
-    sectionTitle: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  section: {
+    marginBottom: 20,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
 
 export default HomeScreen;
